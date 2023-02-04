1d90aed538e0875cbd83aba16a5c33c374fcf8ce
Vlad Chirila <vlad.chirila@live.com>
2023-02-04 13:58:25 +0200

    .

--- ChatGPT/Program.cs
+++ ChatGPT/Program.cs
@@ -7,23 +7,26 @@ using System.Text;
 using Newtonsoft.Json;
 
 if (args.Length > 0){
-    HttpClient client = new HttpClient();
-     var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \""+ args[0] +"\",\"temperature\": 1,\"max_tokens\": 2000}",
-        Encoding.UTF8, "application/json");
-    
-    client.DefaultRequestHeaders.Add("authorization", "Bearer `API KEY`");
-    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
-    string responseString = await response.Content.ReadAsStringAsync();
-
-try{
-    var chatGPTResponse = JsonConvert.DeserializeObject<dynamic>(responseString);
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine(chatGPTResponse!.choices[0].text);
-}catch(JsonException ex){
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(ex.Message);
-}
+        HttpClient client = new HttpClient();
+        var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \""+ args[0] +"\",\"temperature\": 1,\"max_tokens\": 2000}",
+            Encoding.UTF8, "application/json");
+        
+        client.DefaultRequestHeaders.Add("authorization", "Bearer `API KEY`");
+        HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+        string responseString = await response.Content.ReadAsStringAsync();
+        Console.ForegroundColor = ConsoleColor.White;
+    try{
+        var chatGPTResponse = JsonConvert.DeserializeObject<dynamic>(responseString);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(chatGPTResponse!.choices[0].text);
+        Console.ForegroundColor = ConsoleColor.White;
+    }catch(JsonException ex){
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(ex.Message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
 }else{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Wrong input. Arguments missing.");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Wrong input. Arguments missing.");
+Console.ForegroundColor = ConsoleColor.White;
 }
