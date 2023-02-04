59cd2b116a2dd93f7a59f352457aac66791bd592
Vlad Chirila <vlad.chirila@live.com>
2023-02-04 13:42:59 +0200

    Simple client for ChatGPT API with deserialized response text

--- ChatGPT/Program.cs
+++ ChatGPT/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿//  ~~~~~~~~~~~~~~~~~ Simple Chat GPT usage of API ~~~~~~~~~~~~~~~~~~~
+//
+// Do not forget to change `API KEY` with your api key from OPENAI.COM
+//
+
+using System.Text;
+using Newtonsoft.Json;
+
+if (args.Length > 0){
+    HttpClient client = new HttpClient();
+     var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \""+ args[0] +"\",\"temperature\": 1,\"max_tokens\": 2000}",
+        Encoding.UTF8, "application/json");
+    
+    client.DefaultRequestHeaders.Add("authorization", "Bearer `API KEY`");
+    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+    string responseString = await response.Content.ReadAsStringAsync();
+
+try{
+    var chatGPTResponse = JsonConvert.DeserializeObject<dynamic>(responseString);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(chatGPTResponse!.choices[0].text);
+}catch(JsonException ex){
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(ex.Message);
+}
+}else{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Wrong input. Arguments missing.");
+}
